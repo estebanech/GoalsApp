5a5742c3739bb6a60e76d5be772c8f720db5a7c8
Esteban Echeverri <eechev17@eafit.edu.co>
2021-06-23 03:42:37 -0400

    removing key file

--- helpers/firebase-credentials.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-        apiKey: "AIzaSyCWdGyw23ICDqgygashLai5EI0dmY7oJSc",
-        authDomain: "goalsapp-7d5ce.firebaseapp.com",
-        projectId: "goalsapp-7d5ce",
-        storageBucket: "goalsapp-7d5ce.appspot.com",
-        messagingSenderId: "1025802325170",
-        appId: "1:1025802325170:web:b364a1efef88a9c1e28dab",
-        measurementId: "G-QL3LK5QGHX"
-      };
